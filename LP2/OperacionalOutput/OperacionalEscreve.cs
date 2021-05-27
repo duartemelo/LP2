17dd26266ee1d3b3a985087cdc81259057c3e52e
duartemelo <duarteribeirodemelo@hotmail.com>
2021-05-27 01:56:47 +0100

    OperacionalOutput

---
+++ LP2/OperacionalOutput/OperacionalEscreve.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OperacionalBO;
+using System.Collections.Generic;
+
+namespace OperacionalOutput
+{
+    public class OperacionalEscreve
+    {
+        public static void MostraOperacional (Operacional o)
+        {
+            Console.WriteLine(o.ToString());
+        }
+
+        public static void MostraOperacionais (List<Operacional> operacionais)
+        {
+            foreach (Operacional operacional in operacionais)
+            {
+                MostraOperacional(operacional);
+            }
+        }
+    }
+}
